af4a1fde48b388b7a076d5d7081a38a3e5f5795f
zGraund <leo.gaud@hotmail.it>
2026-02-20 02:33:56 +0100

    added tag datagen

--- src/main/java/com/github/zgraund/createreautomated/datagen/DataGenerators.java
+++ src/main/java/com/github/zgraund/createreautomated/datagen/DataGenerators.java
@@ -8,6 +8,8 @@ import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import javax.annotation.Nonnull;
@@ -21,9 +23,13 @@ public class DataGenerators {
     public static void gatherData(@Nonnull GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+
+        BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
+        generator.addProvider(event.includeServer(), blockTagsProvider);
     }
 }
---
+++ src/main/java/com/github/zgraund/createreautomated/datagen/ModBlockTagProvider.java
@@ -0,0 +1,25 @@
+package com.github.zgraund.createreautomated.datagen;
+
+import com.github.zgraund.createreautomated.CreateReAutomated;
+import com.github.zgraund.createreautomated.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, CreateReAutomated.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(@Nonnull HolderLookup.Provider provider) {
+        tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(ModBlocks.EXTRACTOR.get());
+    }
+}
