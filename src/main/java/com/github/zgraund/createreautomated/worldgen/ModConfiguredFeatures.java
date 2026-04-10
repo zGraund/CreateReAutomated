6c4afd96372ccf3367a997c895edb3c01fd4ceb0
zGraund <leo.gaud@hotmail.it>
2026-04-11 00:42:50 +0200

    added zinc node to worldgen

--- src/main/java/com/github/zgraund/createreautomated/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/github/zgraund/createreautomated/worldgen/ModConfiguredFeatures.java
@@ -3,6 +3,7 @@ package com.github.zgraund.createreautomated.worldgen;
 import com.github.zgraund.createreautomated.CreateReAutomated;
 import com.github.zgraund.createreautomated.registry.ModBlocks;
 import com.github.zgraund.createreautomated.registry.ModFeatures;
+import com.simibubi.create.AllBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
@@ -24,11 +25,13 @@ public class ModConfiguredFeatures {
     public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
         List<OreConfiguration.TargetBlockState> overworld = List.of(
                 OreConfiguration.target(new BlockMatchTest(Blocks.COPPER_ORE), ModBlocks.COPPER_NODE.get().unstable()),
+                OreConfiguration.target(new BlockMatchTest(AllBlocks.ZINC_ORE.get()), ModBlocks.ZINC_NODE.get().unstable()),
                 OreConfiguration.target(new BlockMatchTest(Blocks.IRON_ORE), ModBlocks.IRON_NODE.get().unstable()),
                 OreConfiguration.target(new BlockMatchTest(Blocks.GOLD_ORE), ModBlocks.GOLD_NODE.get().unstable()),
                 OreConfiguration.target(new BlockMatchTest(Blocks.DIAMOND_ORE), ModBlocks.DIAMOND_NODE.get().unstable()),
 
                 OreConfiguration.target(new BlockMatchTest(Blocks.DEEPSLATE_COPPER_ORE), ModBlocks.DEEPSLATE_COPPER_NODE.get().unstable()),
+                OreConfiguration.target(new BlockMatchTest(AllBlocks.DEEPSLATE_ZINC_ORE.get()), ModBlocks.DEEPSLATE_ZINC_NODE.get().unstable()),
                 OreConfiguration.target(new BlockMatchTest(Blocks.DEEPSLATE_IRON_ORE), ModBlocks.DEEPSLATE_IRON_NODE.get().unstable()),
                 OreConfiguration.target(new BlockMatchTest(Blocks.DEEPSLATE_GOLD_ORE), ModBlocks.DEEPSLATE_GOLD_NODE.get().unstable()),
                 OreConfiguration.target(new BlockMatchTest(Blocks.DEEPSLATE_DIAMOND_ORE), ModBlocks.DEEPSLATE_DIAMOND_NODE.get().unstable())
