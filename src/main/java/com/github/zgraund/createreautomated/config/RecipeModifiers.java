2acacd20c7357c70bd397211159402153368ead0
zGraund <leo.gaud@hotmail.it>
2026-06-17 01:52:15 +0200

    new configs

--- src/main/java/com/github/zgraund/createreautomated/config/RecipeModifiers.java
+++ src/main/java/com/github/zgraund/createreautomated/config/RecipeModifiers.java
@@ -1,8 +1,11 @@
 package com.github.zgraund.createreautomated.config;
 
 import net.createmod.catnip.config.ConfigBase;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 
 import javax.annotation.Nonnull;
+import java.text.DecimalFormat;
 
 public class RecipeModifiers extends ConfigBase {
     public final ConfigFloat duration = multi("duration", "Extract duration multiplier.");
@@ -10,6 +13,16 @@ public class RecipeModifiers extends ConfigBase {
     public final ConfigFloat fluid = multi("fluid", "Fluid consumption multiplier.");
     public final ConfigFloat node = multi("nodeConsumption", "Node consumption multiplier.");
     public final ConfigFloat output = multi("output", "Item output multiplier.");
+
+    public static Component formatModifier(@Nonnull ConfigFloat mod) {
+        return formatModifier(mod.getF());
+    }
+
+    public static Component formatModifier(float mod) {
+        if (mod == 1)
+            return Component.empty();
+        return Component.literal(new DecimalFormat(" (###.###x)").format(mod)).withStyle(mod < 1f ? ChatFormatting.DARK_GREEN : ChatFormatting.RED);
+    }
 
     public ConfigFloat multi(String name, String... comments) {
         return f(1, 0, 100, name, comments);
