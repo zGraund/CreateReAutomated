ad6d975110679ebbfe0035addc0d13a877b1c88d
zGraund <leo.gaud@hotmail.it>
2026-06-25 00:06:19 +0200

    added option to encased node feature to disallow multiple nodes in the same ore vein

--- src/main/java/com/github/zgraund/createreautomated/worldgen/feature/EncasedNodeConfiguration.java
+++ src/main/java/com/github/zgraund/createreautomated/worldgen/feature/EncasedNodeConfiguration.java
@@ -20,29 +20,48 @@ public class EncasedNodeConfiguration extends ReplaceBlockConfiguration {
             NeoForgeExtraCodecs.xor(
                     Worldgen.NodeGroup.CODEC.fieldOf("group"),
                     IntProvider.codec(0, 6).fieldOf("faces")
-            ).forGetter(config -> config.either)
+            ).forGetter(config -> config.either),
+            Codec.BOOL.optionalFieldOf("allowSameVein", false).forGetter(config -> config.allowSameVein)
     ).apply(inst, EncasedNodeConfiguration::new));
 
     private final Either<Worldgen.NodeGroup, IntProvider> either;
+    private final boolean allowSameVein;
 
-    public EncasedNodeConfiguration(List<OreConfiguration.TargetBlockState> targetStates, Either<Worldgen.NodeGroup, IntProvider> config) {
+    public EncasedNodeConfiguration(List<OreConfiguration.TargetBlockState> targetStates, Either<Worldgen.NodeGroup, IntProvider> config, boolean allowSameVein) {
         super(targetStates);
         this.either = config;
+        this.allowSameVein = allowSameVein;
     }
 
     @Nonnull
     @Contract("_, _ -> new")
     public static EncasedNodeConfiguration of(List<OreConfiguration.TargetBlockState> targetStates, IntProvider faces) {
-        return new EncasedNodeConfiguration(targetStates, Either.right(faces));
+        return of(targetStates, faces, false);
+    }
+
+    @Nonnull
+    @Contract("_, _, _ -> new")
+    public static EncasedNodeConfiguration of(List<OreConfiguration.TargetBlockState> targetStates, IntProvider faces, boolean allowSameVein) {
+        return new EncasedNodeConfiguration(targetStates, Either.right(faces), allowSameVein);
     }
 
     @Nonnull
     @Contract("_, _ -> new")
     public static EncasedNodeConfiguration of(List<OreConfiguration.TargetBlockState> targetStates, Worldgen.NodeGroup group) {
-        return new EncasedNodeConfiguration(targetStates, Either.left(group));
+        return of(targetStates, group, false);
+    }
+
+    @Nonnull
+    @Contract("_, _, _ -> new")
+    public static EncasedNodeConfiguration of(List<OreConfiguration.TargetBlockState> targetStates, Worldgen.NodeGroup group, boolean allowSameVein) {
+        return new EncasedNodeConfiguration(targetStates, Either.left(group), allowSameVein);
     }
 
     public int getFaces(RandomSource random) {
         return either.map(group -> group.getConfig().requiredFaces.get(), supp -> supp.sample(random));
+    }
+
+    public boolean allowSameVein() {
+        return allowSameVein;
     }
 }
--- src/main/java/com/github/zgraund/createreautomated/worldgen/feature/EncasedNodeFeature.java
+++ src/main/java/com/github/zgraund/createreautomated/worldgen/feature/EncasedNodeFeature.java
@@ -6,8 +6,10 @@ import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.neoforged.neoforge.common.util.TriState;
 
 import javax.annotation.Nonnull;
+import java.util.*;
 
 public class EncasedNodeFeature extends Feature<EncasedNodeConfiguration> {
     public EncasedNodeFeature() {
@@ -20,9 +22,18 @@ public class EncasedNodeFeature extends Feature<EncasedNodeConfiguration> {
         BlockPos blockpos = context.origin();
         EncasedNodeConfiguration encasedNodeConfiguration = context.config();
         BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
+        TriState found = TriState.DEFAULT;
 
         for (OreConfiguration.TargetBlockState targetState : encasedNodeConfiguration.targetStates) {
             if (targetState.target.test(worldgenlevel.getBlockState(blockpos), context.random())) {
+
+                if (!encasedNodeConfiguration.allowSameVein()) {
+                    if (found.isDefault())
+                        found = findNode(context);
+                    if (found.isTrue())
+                        return true;
+                }
+
                 int requiredFaces = encasedNodeConfiguration.getFaces(context.random());
                 if (requiredFaces > 0) {
                     for (Direction direction : Direction.values()) {
@@ -41,5 +52,40 @@ public class EncasedNodeFeature extends Feature<EncasedNodeConfiguration> {
         }
 
         return true;
+    }
+
+    public TriState findNode(@Nonnull FeaturePlaceContext<EncasedNodeConfiguration> context) {
+        int maxDepth = 8;
+
+        EncasedNodeConfiguration encasedNodeConfiguration = context.config();
+        WorldGenLevel level = context.level();
+        BlockPos originPos = context.origin();
+
+        Set<BlockPos> visited = new HashSet<>(List.of(originPos));
+        Deque<BlockPos> deque = new ArrayDeque<>(List.of(originPos));
+
+        while (!deque.isEmpty()) {
+            BlockPos currentPos = deque.pop();
+            BlockPos.MutableBlockPos nextPos = new BlockPos.MutableBlockPos();
+            for (Direction direction : Direction.values()) {
+                nextPos.setWithOffset(currentPos, direction);
+
+                for (OreConfiguration.TargetBlockState targetState : encasedNodeConfiguration.targetStates) {
+                    if (level.getBlockState(nextPos).is(targetState.state.getBlock())) {
+                        return TriState.TRUE;
+                    }
+
+                    if (targetState.target.test(level.getBlockState(nextPos), level.getRandom()) && !visited.contains(nextPos) &&
+                        nextPos.distManhattan(originPos) <= maxDepth) {
+                        BlockPos nextCopy = nextPos.immutable();
+                        deque.add(nextCopy);
+                        visited.add(nextCopy);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return TriState.FALSE;
     }
 }
